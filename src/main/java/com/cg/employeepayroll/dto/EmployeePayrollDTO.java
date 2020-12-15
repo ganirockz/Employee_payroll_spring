9720f96bd9998ad91fd49864663c257f04fe072f
ganirockz <ganeshrapeti8@gmail.com>
2020-12-16 00:55:12 +0530

    [Ganesh]Added: validation to name,gender and salary

--- src/main/java/com/cg/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/cg/employeepayroll/dto/EmployeePayrollDTO.java
@@ -4,15 +4,21 @@ import com.cg.employeepayroll.model.EmployeePayrollData;
 
 import java.time.LocalDate;
 import java.util.*;
+
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class EmployeePayrollDTO {
 	
 	private Long id;
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee name Invalid")
 	private String name;
+	@Pattern(regexp = "[0-9]+", message ="salary should be a number")
 	private String basicPay;
 	private LocalDate startDate;
+	@Pattern(regexp="[MF]",message="gender should be single character")
 	private Character gender;
 	
 	public EmployeePayrollDTO(EmployeePayrollData employeePayroll) {
